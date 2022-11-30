6aa5295228e9f0b86a72e510378660ce95608f9a
solaz <sola.zhou@thoughtworks.com>
2022-11-30 17:30:22 +1100

    Move calc logics into category enum

--- src/main/java/com/thoughtworks/movierental/Category.java
+++ src/main/java/com/thoughtworks/movierental/Category.java
@@ -1,5 +1,38 @@
 package com.thoughtworks.movierental;
 
 public enum Category {
-    CHILDRENS, REGULAR, NEW_RELEASE;
+    CHILDRENS {
+        @Override
+        double getRentalPrice(int daysRented) {
+            double price = 1.5;
+            if (daysRented > 3)
+                price += (daysRented - 3) * 1.5;
+            return price;
+        }
+    },
+    REGULAR {
+        @Override
+        double getRentalPrice(int daysRented) {
+            double price = 2;
+            if (daysRented > 2)
+                price += (daysRented - 2) * 1.5;
+            return price;
+        }
+    }, NEW_RELEASE {
+        @Override
+        double getRentalPrice(int daysRented) {
+            return daysRented * 3;
+        }
+        @Override
+        public int getFrequentRenterPoints(int daysRented) {
+            if (daysRented > 1)  return 2;
+            return 1;
+        }
+    };
+
+    abstract double getRentalPrice(int daysRented);
+
+    public int getFrequentRenterPoints(int daysRented) {
+        return 1;
+    }
 }
--- src/main/java/com/thoughtworks/movierental/Movie.java
+++ src/main/java/com/thoughtworks/movierental/Movie.java
@@ -23,31 +23,10 @@ public class Movie {
     }
 
     public double getRentalPrice(int daysRented) {
-       double price = 0;
-       switch (category) {
-           case REGULAR:
-               price += 2;
-               if (daysRented > 2)
-                   price += (daysRented - 2) * 1.5;
-               break;
-           case NEW_RELEASE:
-               price += daysRented * 3;
-               break;
-           case CHILDRENS:
-               price += 1.5;
-               if (daysRented > 3)
-                   price += (daysRented - 3) * 1.5;
-               break;
-       }
-       return price;
+       return category.getRentalPrice(daysRented);
    }
 
     public int getFrequentRenterPoints(int daysRented) {
-        int frequentRenterPoints = 1;
-        // add bonus for a two day new release rental
-        if ((category == Category.NEW_RELEASE)
-                &&
-                daysRented > 1) frequentRenterPoints++;
-        return frequentRenterPoints;
+       return category.getFrequentRenterPoints(daysRented);
     }
 }
